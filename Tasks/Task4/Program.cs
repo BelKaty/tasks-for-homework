e670003c3b39b5e290b5fcc3511181e1a9c025c2
KatBel <sssecret@mail.ru>
2022-09-19 19:39:43 +0300

    add Task4

---
+++ Tasks/Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите первое значение:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе значение:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье значение:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    if (numberA > numberC)
+    {
+        Console.Write(numberA + " - самое большое число из заданных");
+    }
+    else
+    {
+        Console.Write(numberC + " - самое большое число из заданных");
+    }
+}
+else
+{
+    if (numberB > numberC)
+    {
+        Console.Write(numberB + " - самое большое число из заданных");
+    }
+    else
+    {
+        Console.Write(numberC + " - самое большое число из заданных");
+    }
+}
