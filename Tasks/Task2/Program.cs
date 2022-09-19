7d37e62f95b2aea6d9c4e45fb47b69f4bd5532ff
KatBel <sssecret@mail.ru>
2022-09-19 19:32:49 +0300

    add Task2

---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое значение:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе значение:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.Write(a + " > " + b);
+}
+else
+{
+    Console.Write(a + " < " + b);
+}
