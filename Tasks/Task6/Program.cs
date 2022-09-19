169d83bb8b38c481b17d9f9e886a0cbc99469fb0
KatBel <sssecret@mail.ru>
2022-09-19 19:42:58 +0300

    add Task6

---
+++ Tasks/Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write(number + " - число четное");
+  }
+else
+{
+    Console.Write(number + " - число нечетное");
+}
