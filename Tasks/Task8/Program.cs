6f325945801707f69497e338b778cb16855a9f38
KatBel <sssecret@mail.ru>
2022-09-19 19:48:59 +0300

    add Task8

---
+++ Tasks/Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = 0;
+Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (n = 0; n <= N; n = n + 2)
+{
+    if (n % 2 == 0)
+    {
+        Console.Write(n + " ");        
+    }
+    else
+    {
+        n = n + 1;
+    }
+}
+Console.Write("- все четные числа в диапазоне от 0 до " + N);
