9296daf88931dea36de312f15cec39968b86017f
KatBel <sssecret@mail.ru>
2022-09-24 10:55:46 +0300

    Task111

---
+++ Tasks/Task111/Program.cs
@@ -0,0 +1,59 @@
+﻿// 17. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер 
+// четверти плоскости, в которой находится эта точка.
+int getQuarterFromCoordinate(int coordinateX, int coordinateY)
+{
+    int result = 0;
+    if (coordinateX > 0 && coordinateY > 0)
+    {
+        result = 1;
+    }
+    else if (coordinateX < 0 && coordinateY > 0)
+    {
+        result = 2;
+    }
+    else if (coordinateX < 0 && coordinateY < 0)
+    {
+        result = 3;
+    }
+    else if (coordinateX > 0 && coordinateY < 0)
+    {
+        result = 4;
+    }
+    return result;
+}
+int getCoordinateFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine,out result);
+        if(result == 0 && userLine != "0") 
+        {
+            Console.WriteLine($"Введите целое число вы ввели {userLine}");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+int userCoordinateX = 0;
+int userCoordinateY = 0;
+Console.WriteLine("Введите координаты");
+userCoordinateX = getCoordinateFromUser("X: ");
+userCoordinateY = getCoordinateFromUser("Y: ");
+
+int quarter = 
+int getQuarterNumberFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation}: ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result == 0)
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число от 1 до 4. {userInformation}: ");
+    }
+    return result;
+}
