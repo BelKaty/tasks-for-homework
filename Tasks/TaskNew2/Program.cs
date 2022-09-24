1cd259ca6d0359a9cb66776f762b92b4f258e030
KatBel <sssecret@mail.ru>
2022-09-24 11:12:16 +0300

    TaskNew2

---
+++ Tasks/TaskNew2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+78 -> 8 
+12-> 2 
+85 -> 8
+*/
+int getRandomNumberFromRange(int minBorder, int maxBorder)
+{
+    int result = new int();
+    result = new Random().Next(minBorder, maxBorder + 1);
+    return result;
+}
+
+
+int randomNumber = getRandomNumberFromRange(100,999);
+Console.WriteLine($"Случайное значение {randomNumber}");
+int number3 = randomNumber%10;
+int number1 = randomNumber/100;
+int resultnumber = number1*10+number3;
+Console.WriteLine($"Результат {resultnumber}");
